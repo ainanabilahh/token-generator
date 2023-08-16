87122ff40d8d6da512dee34884541be8c8401a6f
Aina Nabilah <ainanabilahh@gmail.com>
2023-08-16 22:07:05 +0800

    add upload function then generate random id

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ and secure secret keys.
 import datetime
 import random
 import secrets
+import shutil
+from pathlib import Path
 
 import jwt
-from fastapi import FastAPI, Header, HTTPException
+from fastapi import FastAPI, File, Form, Header, HTTPException, UploadFile
 
 from user import User
 
@@ -64,7 +66,7 @@ def verify_token(token: str) -> bool:
     """
     try:
         # Decode the token using the same secret key and algorithm used to encode it
-        decoded_token = jwt.decode(
+        jwt.decode(
             token, SECRET_KEY, algorithms=[ALGORITHM])
 
         # Optional: You can add additional checks, such as verifying the 'sub' (subject) field
@@ -83,7 +85,7 @@ def verify_token(token: str) -> bool:
 
 
 @app.post("/token/")
-def generate_token(user: User):
+def generate_token(username: str = Form(...), password: str = Form(...)):
     """Generate an access token for the provided user.
 
     This endpoint expects a JSON object with the user's username and password.
@@ -95,12 +97,15 @@ def generate_token(user: User):
     Returns:
         dict: A dictionary containing the access token and token type.
     """
+
+    user = User(username=username, password=password)
+
     token = create_token(user)
-    return {"access_token": token, "token_type": "bearer"}
+    return {"ticket": token, "token_type": "bearer"}
 
 
 @app.post("/random/")
-def generate_random_id(authorization: str = Header(...)) -> str:
+def generate_random_id() -> str:
     """
     Generate a Random Numeric ID
 
@@ -118,6 +123,29 @@ def generate_random_id(authorization: str = Header(...)) -> str:
     """
 
     # Extract the token from the header value (assuming the format is "Bearer <token>")
+    return ''.join(random.choice('0123456789') for _ in range(8))
+
+
+@app.post("/upload/")
+async def upload_file(authorization: str = Header(...), file: UploadFile = File(...)):
+    """
+    Upload a File and Generate a Random ID
+
+    Accept a file upload and an authorization token. It verifies the token, saves the file
+    to the "data" folder, and returns a randomly generated ID.
+
+    Args:
+        authorization (str): The authorization token provided in the request header.
+        file (UploadFile): The file to be uploaded, provided as multipart data.
+
+    Returns:
+        dict: A dictionary containing the random ID and the path where the file was saved.
+
+    Raises:
+        HTTPException: If the token is invalid or if there is an error in processing the file.
+    """
+    # Extract the token from the header and verify it
+    # Extract the token from the header value (assuming the format is "Bearer <token>")
     token = authorization.split(
         " ")[1] if " " in authorization else authorization
 
@@ -126,4 +154,24 @@ def generate_random_id(authorization: str = Header(...)) -> str:
     if not verify_token(token):
         raise HTTPException(status_code=401, detail="Invalid token")
 
-    return ''.join(random.choice('0123456789') for _ in range(8))
+    # Check if the filename is provided
+    if file.filename is None:
+        raise HTTPException(status_code=400, detail="Filename is missing")
+
+
+    # Define the path to the "data" folder
+    data_folder = Path("data")
+    data_folder.mkdir(exist_ok=True)
+
+    # Define the path to save the uploaded file
+    file_path = data_folder / file.filename
+
+    # Save the uploaded file
+    with file_path.open("wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    # Generate a random ID
+    random_id = generate_random_id()
+
+    # Return the random ID and the path where the file was saved
+    return {"id": random_id}
